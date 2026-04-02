d0e28da16d148fa0a350daa42e461c96ad471e9e
Mayank Gupta <150025164+MayankGupt10@users.noreply.github.com>
2026-04-02 22:39:21 +0530

    Time: 287 ms (50.4%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,3 +1,8 @@
-SELECT name, population, area
-FROM World
-WHERE area >= 3000000 OR population >= 25000000;
+select name, population, area 
+from World 
+where area >= 3000000 OR population >= 25000000;
+
+-- select name, population, area 
+-- from World 
+-- where area >= 3000000 and population >= 25000000;
+
