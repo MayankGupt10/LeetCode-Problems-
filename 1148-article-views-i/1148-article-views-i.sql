4bbc6a863d0da8badf3a9344435063bb269e3252
Mayank Gupta <150025164+MayankGupt10@users.noreply.github.com>
2026-04-17 23:51:09 +0530

    Time: 405 ms (54.83%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,9 @@
-select DISTINCT author_id AS id 
-FROM Views 
-WHERE author_id = viewer_id 
+-- select DISTINCT author_id AS id 
+-- FROM Views 
+-- WHERE author_id = viewer_id 
+-- order by id;
+
+SELECT DISTINCT author_id AS id 
+from Views 
+where author_id = viewer_id 
 order by id;
